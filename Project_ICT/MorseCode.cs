0b880f24c6853596f925a768ab94678a62e0f102
DESKTOP-SOB6BIC\32473 <woutlibbrecht88@gmail.com>
2023-11-23 12:21:21 +0100

    Nieuwe klasse aangemaakt

---
+++ Project_ICT/MorseCode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ICT
+{
+    internal class MorseCode
+    {
+        private string code;
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public string Vertaal(string text)
+        {
+            foreach (char a in text)
+            {
+                string i = a.ToString();
+                Code = Code + Codeer(i);
+            }
+            return code;
+        }
+
+        private string Codeer(string i)
+        {
+            string vertaling;
+            if (i == "a")
+                vertaling = ".-";
+            if (i == "b")
+                vertaling = "-...";
+            if (i == "c")
+                vertaling = "-.-.";
+
+        } 
+
+    }
+}
+
+
+// A = .-
+// B = - ...
+// C = -.-.
+// D = - ..
+// E = .
+// F = ..-.
+// G = --.
+// H = ....
+// I = ..
+// J = .-- -
+// K = -.-
+// L = .- ..
+// M --
+// N = -.
+// O =-- -
+// P = .--.
+// Q = --.-
+// R = .-.
+// S = ...
+// T = -
+// U = ..-
+// V = ...-
+// W = .--
+// X = - ..-
+// Y = -.--
+// Z = --..
+// 0 = -----
+// 1 = .----
+// 2 = ..---
+// 3 = ...--
+// 4 = ....-
+// 5 = .....
+// 6 = - ....
+// 7 = --...
+// 8 = -- - ..
+// 9 = ----.
--- Project_ICT/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Project_ICT/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace Project_ICT {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Project_ICT;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Project_ICT;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
