31711afcbd1265b8a3989b917fb512d63fbe586c
DESKTOP-SOB6BIC\32473 <woutlibbrecht88@gmail.com>
2023-11-23 12:04:47 +0100

    GUI aangemaakt en Seriele poort toegevoegd

--- Project_ICT/MainWindow.xaml.cs
+++ Project_ICT/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Project_ICT
 {
     /// <summary>
@@ -20,10 +22,52 @@ namespace Project_ICT
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort _serialPort;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            _serialPort = new SerialPort();
+
+            
+
+            cbxCOMPoort.Items.Add("None");
+            foreach (string s in SerialPort.GetPortNames())
+                cbxCOMPoort.Items.Add(s);
         }
-        //jirezqjfqdjfdsqklfjdsqkfjsqjkfjdsqklf
+
+        private void cbxCOMPoort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            {
+                if (_serialPort != null)
+                {
+                    if (_serialPort.IsOpen)
+                        _serialPort.Close();
+
+                    if (cbxCOMPoort.SelectedItem.ToString() != "None")
+                    {
+                        _serialPort.PortName = cbxCOMPoort.SelectedItem.ToString();
+                        _serialPort.Open();
+                        tbkMorseCode.IsEnabled = true;
+                        tbxTekst.IsEnabled = true;
+                        btnVertaal.IsEnabled = true;
+                    }
+                    if (cbxCOMPoort.SelectedItem.ToString() == "None")
+                    {
+                        tbkMorseCode.IsEnabled = false;
+                        tbxTekst.IsEnabled = false;
+                        btnVertaal.IsEnabled = false;
+                    }
+                }
+
+            }
+
+        }
+
+        private void btnVertaal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
